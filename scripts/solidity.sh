17619bae9e9546a2eb0c123403b018bcdd1ce408
satyam <satyam@secureblocks.io>
2018-08-14 10:35:01 +0530

    again again trying

--- scripts/solidity.sh
+++ scripts/solidity.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-echo "doing step 1"
 echo -ne '\n' | sudo add-apt-repository ppa:ethereum/ethereum
-echo "doing step 2"
 sudo apt-get -y update
-echo "doing step 3"
 sudo apt-get -y install solc
-echo "Installing solc ...."
 solc --version
