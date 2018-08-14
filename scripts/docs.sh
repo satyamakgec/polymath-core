33808b7ccbc0fd6c8e63d9c1b4bdd81fab923118
satyam <satyam@secureblocks.io>
2018-08-14 08:39:36 +0530

    fix2

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -60,7 +60,7 @@ reject_docs() {
     exit 0
 }
 echo "doing step 1"
-cat < (echo "") | sudo add-apt-repository ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum
 echo "doing step 2"
 sudo apt-get update -y
 echo "doing step 3"
